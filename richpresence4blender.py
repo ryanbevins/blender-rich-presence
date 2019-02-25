9ea94329a0b67431aca164dae44ec54c6ba4eed1
ryanbevins <ryanabevins@gmail.com>
2019-02-25 01:59:50 -0800

    Made usable

--- richpresence4blender.py
+++ richpresence4blender.py
@@ -1,8 +1,8 @@
 bl_info = {
 	"name": "Discord Rich Presence",
 	"description": "Adds Rich Presence support to Blender",
-	"author": "@AlexApps#9295, @lvxejay#9771",
-	"version": (0, 1),
+	"author": "@AlexApps#9295, @lvxejay#9771, @Ryan()#5875",
+	"version": (0, 8),
 	"blender": (2, 79, 0),
 	"location": "",
 	"warning": "Extremely Unstable - Still in development!",
@@ -15,9 +15,9 @@ import rpc
 import time
 import bpy
 import threading
+import bmesh
 
-def register():
-	print('Placeholder')
+
 def unregister():
 	print('Placeholder')
 
@@ -26,24 +26,54 @@ rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)
 time.sleep(5)
 start_time = time.time()
 filename = 'test.blend'
-version_no = '2.79'
+version_no = bpy.app.version_string
 
-def updatePresence():
-	threading.Timer(30, updatePresence).start()
-	
-	activity = {
-		"details": "Using Blender " + version_no,
-		"state": "Working on " + filename,
-		"timestamps": {
-			"start": start_time
-		},
-		"assets": {
-			"small_text": "Blender " + version_no,
-			"small_image": "blender_logo",
-			"large_text": filename,
-			"large_image": "pretty_render"
+print("test")
+
+class presencePanel(bpy.types.Panel):
+	"""Creates a Panel in the Object properties window"""
+	bl_label = "Discord Rich Presence"
+	bl_idname = "OBJECT_PT_hello"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'TOOLS'
+
+	def draw(self, context):
+		layout = self.layout
+		row = layout.row()
+		row.operator("wm.update_presence")
+
+class presenceOperator(bpy.types.Operator):
+	bl_idname = "wm.update_presence"
+	bl_label = "Update Discord Presence"
+	def execute(self, context):
+		print("test2");
+		activity = {
+			"details": "Using Blender " + version_no,
+			"state": bpy.path.basename(bpy.context.blend_data.filepath) + " | Poly Count: " + str(getPolyCount()),
+
+			"timestamps": {
+				"start": start_time
+			},
+			"assets": {
+				"small_text": "Blender " + version_no,
+				"small_image": "blender_logo",
+				"large_text": filename,
+				"large_image": "pretty_render"
+			}
 		}
-	}
-	rpc_obj.set_activity(activity)
-	
-updatePresence()
+		rpc_obj.set_activity(activity)
+		return {'FINISHED'}
+
+def getPolyCount():
+	polyCount = 0
+	for obj in bpy.context.scene.objects:
+		if obj.type == 'MESH':
+			mesh = obj.data
+			polyCount += len(mesh.polygons)
+	return polyCount
+
+bpy.utils.register_class(presenceOperator)
+bpy.utils.register_class(presencePanel)
+
+def register():
+	bpy.utils.register_module(__name__)
